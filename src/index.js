5f0dcae719e28b43640a87d9b6bc6d1372c131ea
Grillo <Jory.Grillo@imglobal.com>
2017-03-28 14:14:39 -0400

    Final - Had to end here. Does not recognize 'export' in mockDataSchema.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-var css = require('./index.css'); // eslint-disable-line no-unused-vars
+import {getUsers} from './api/userApi';
 
-var numeral = require('numeral');
+// Populate table of users via API call.
+getUsers().then(result => {
+    let usersBody = "";
 
-const courseValue = numeral(1000).format('$0,0.00');
-//debugger;
-console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+    result.forEach(user => {
+        usersBody+= `<tr>
+        <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
+        <td>${user.id}</td>
+        <td>${user.firstName}</td>
+        <td>${user.lastName}</td>
+        <td>${user.email}</td>
+        </tr>`
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+});
